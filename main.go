d840d98854f3f2d983186b7dcb25db26c6aaa2a1
ipigtw <diegogamingtw@gmail.com>
2023-12-07 00:53:34 +0800

    made this shit

--- main.go
+++ main.go
@@ -11,19 +11,30 @@ type Worde struct {
 	Zhuyin string `json:"zhuyin"`
 }
 func main() {
-	save("靠", "ㄎㄠˋ")
-	fmt.Println(checkWord("嗨"))
+	var word string
+	fmt.Print("Enter a word to check...")
+	fmt.Scan(&word)
+	check, zhu := check(word)
+	if !check {
+		var zhuyin string
+		fmt.Print("Word not found! Please enter the zhuyin...")
+		fmt.Scan(&zhuyin)
+		save(word, zhuyin)
+	}else {
+		fmt.Print("Word found! The zhuyin is "+zhu)
+	}
 }
-func checkWord(word string) bool {
+
+func check(word string) (bool, string) {
 	var tmp []Worde
 	readfile, _ := os.ReadFile("data.json")
 	json.Unmarshal(readfile, &tmp)
 	for _, w := range tmp {
 		if w.Word == word {
-			return true
+			return true, w.Zhuyin
 		}
 	}
-	return false
+	return false, ""
 }
 
 func save(word string, zhuyin string) {
@@ -34,7 +45,11 @@ func save(word string, zhuyin string) {
 	}
 	readfile, _ := os.ReadFile("data.json")
 	json.Unmarshal(readfile, &tmp)
-	
+	for _, w := range tmp {
+		if w == zhi {
+			return
+		}
+	}
 	tmp = append(tmp, zhi)
 	e,_ := json.Marshal(tmp)
 	os.WriteFile("data.json", e, 0644)
