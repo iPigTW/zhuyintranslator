d02c86bc1650fb23e6421370d267c688ea67c5ac
ipigtw <diegogamingtw@gmail.com>
2023-12-07 00:45:31 +0800

    yes

--- main.go
+++ main.go
@@ -3,22 +3,39 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
-type Word struct {
+type Worde struct {
 	Word string `json:"word"`
 	Zhuyin string `json:"zhuyin"`
 }
 func main() {
-	save("幹", "ㄍㄢˋ")
+	save("靠", "ㄎㄠˋ")
+	fmt.Println(checkWord("嗨"))
+}
+func checkWord(word string) bool {
+	var tmp []Worde
+	readfile, _ := os.ReadFile("data.json")
+	json.Unmarshal(readfile, &tmp)
+	for _, w := range tmp {
+		if w.Word == word {
+			return true
+		}
+	}
+	return false
 }
 
 func save(word string, zhuyin string) {
-	zhi := Word {
+	var tmp []Worde
+	zhi := Worde {
 		Word: word,
 		Zhuyin: zhuyin,
 	}
-	j, _ := json.Marshal(zhi)
-	json := string(j)
+	readfile, _ := os.ReadFile("data.json")
+	json.Unmarshal(readfile, &tmp)
 	
+	tmp = append(tmp, zhi)
+	e,_ := json.Marshal(tmp)
+	os.WriteFile("data.json", e, 0644)
 }
